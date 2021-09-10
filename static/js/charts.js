e001bd9688af61ed1c40f8d74f51d0d5c38a7c09
Gabriel Gaucin <>
2021-09-09 21:25:32 -0500

    adjust bar and gauge chart widths

--- static/js/charts.js
+++ static/js/charts.js
@@ -100,7 +100,7 @@ function buildCharts(sample) {
      plot_bgcolor: paper_color,
      font: { color: text_color },
      height: 350,
-     width: 350,
+     width: 300,
      xaxis: { automargin: true },
      yaxis: { automargin: true }
     };
@@ -154,7 +154,7 @@ function buildCharts(sample) {
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
-      width: 350,
+      width: 300,
       height: 350,
       paper_bgcolor: paper_color,
       font: { color: text_color },
