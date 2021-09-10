5d87278649cdd5e38157f412d74edb268df5361a
Gabriel Gaucin <>
2021-09-09 21:37:07 -0500

    adjust well class tags

--- static/js/charts.js
+++ static/js/charts.js
@@ -159,7 +159,7 @@ function buildCharts(sample) {
       height: 350,
       paper_bgcolor: paper_color,
       font: { color: text_color },
-      margin: { b: 10, l: 15, t: 10, r: 10 }
+      margin: { b: 10, l: 15, t: 10, r: 15 }
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
